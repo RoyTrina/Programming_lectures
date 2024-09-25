a1b48b4cc7619ddfb73ce1823ab25d39c253d68b
Trina <44895876+RoyTrina@users.noreply.github.com>
2024-09-25 16:00:37 +0100

    Add files via upload

---
+++ HTML/My website/Introdction.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "UTF-8">
+		<title>The first page</title>
+		<link rel = "stylesheet" href="style.css" type = "text/css" />
+	</head>
+	<body>
+		<h1>The homepage</h1>
+		<p>This is the introductory page of the website.</p>
+	</body>
+</html>
